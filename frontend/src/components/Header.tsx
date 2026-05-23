6437ff2176db3b64037e7874d47145342a34075f
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-05-23 19:39:54 +0200

    add gradient style

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,5 +3,9 @@ type Props = {
 }
 
 export default function Header({ text }: Props) {
-	return <h1 className="text-2xl sm:text-3xl lg:text-4xl text-center font-bold my-2 text-sky-700">{text}</h1>
+	return (
+		<h1 className="mt-12 mb-46 lg:mb-20 sm:my-16 md:my-20 lg:my-24 text-4xl font-extrabold tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-primary via-blue-500 to-info sm:text-5xl md:text-6xl max-w-4xl mx-auto pb-2 text-center">
+			{text}
+		</h1>
+	)
 }
