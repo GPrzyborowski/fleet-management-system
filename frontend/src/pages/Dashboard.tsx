1fd7831967ddd4c3a1f4a68369723cafa32a2487
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-05-24 07:43:45 +0200

    feat: create manager dashboard view

---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { jwtDecode } from 'jwt-decode'
+import PageTransition from '../components/PageTransition'
+import Header from '../components/Header'
+import Navbar from '../components/Navbar'
+import ActionCard from '../components/ActionCard'
+
+interface TokenPayload {
+	userId: number
+	login: string
+	role: string
+	exp: number
+}
+
+export default function Dashboard() {
+	const [login, setLogin] = useState('')
+	const [role, setRole] = useState('')
+
+	const navigate = useNavigate()
+
+	const handleLogout = () => {
+		localStorage.removeItem('token')
+		navigate('/')
+	}
+
+	useEffect(() => {
+		const token = localStorage.getItem('token')
+		if (!token) {
+			return
+		}
+		try {
+			const decoded = jwtDecode<TokenPayload>(token)
+			setLogin(decoded.login)
+			setRole(decoded.role)
+		} catch (err) {
+			console.error(err)
+		}
+	}, [])
+
+	return (
+		<PageTransition>
+			<Navbar logoutHandler={handleLogout} />
+			<Header text={`Hello ${login}`} />
+			{role == 'manager' && (
+				<div className="px-4 sm:px-12 md:px-18 lg:px-34 xl:px-54 xl:mt-34 grid grid-cols-1 gap-4 max-w-5xl mx-auto">
+					<ActionCard title="Employees" description="Employees management panel." icon="icon-[tabler--user]" />
+					<ActionCard title="Vehicles" description="Vehicles management panel." icon="icon-[tabler--truck]" />
+				</div>
+			)}
+		</PageTransition>
+	)
+}
