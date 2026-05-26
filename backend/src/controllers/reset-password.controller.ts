f369e28113f55ce07d35161f91124728ecaa94eb
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-05-26 21:59:54 +0200

    feat: validate token from redis and update password

---
+++ backend/src/controllers/reset-password.controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express'
+import bcrypt from 'bcrypt'
+import prisma from '../config/prisma-client'
+import redis from '../config/redis-client'
+
+export const resetPassword = async (req: Request, res: Response) => {
+	const { token, newPassword }: { token: string; newPassword: string } = req.body
+
+	const userId = await redis.get(`reset:${token}`)
+	if (!userId) {
+		return res.status(400).json({ error: 'Token is invalid or has expired.' })
+	}
+
+	const hash = await bcrypt.hash(newPassword, 10)
+
+	await prisma.users.update({
+		where: { id: Number(userId) },
+		data: { password_hash: hash },
+	})
+
+	await redis.del(`reset:${token}`)
+	return res.status(200).json({ message: 'Successfully reset the password.' })
+}
