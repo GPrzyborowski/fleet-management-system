8a431b5c5317b5fd9c6d660a513ca0b56fe21a82
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-05-30 19:05:55 +0200

    feat: add addVehicle route

--- backend/src/routes/vehicles.routes.ts
+++ backend/src/routes/vehicles.routes.ts
@@ -1,9 +1,10 @@
 import express from 'express'
-import { getVehicles, updateVehicle, deleteVehicle } from '../controllers/vehicles.controller'
+import { getVehicles, addVehicle, updateVehicle, deleteVehicle } from '../controllers/vehicles.controller'
 
 const router = express.Router()
 
 router.get('/vehicles', getVehicles)
+router.post('/vehicles', addVehicle)
 router.delete('/vehicles/:id', deleteVehicle)
 router.patch('/vehicles/:id', updateVehicle)
 
