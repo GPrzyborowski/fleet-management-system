0998c08619cf86d36855e031367261ed75cddbc8
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-05-30 19:19:30 +0200

    feat: create vehicle details page

---
+++ frontend/src/pages/VehicleDetails.tsx
@@ -0,0 +1,10 @@
+import Header from '../components/Header'
+import PageTransition from '../components/PageTransition'
+
+export default function VehicleDetails() {
+	return (
+		<PageTransition>
+			<Header text="Vehicle details" />
+		</PageTransition>
+	)
+}
