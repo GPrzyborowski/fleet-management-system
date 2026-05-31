9db88893b6129e883214e338c64af49276d43223
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-05-31 18:03:45 +0200

    feat: add assignment end route

--- backend/src/routes/assignments.routes.ts
+++ backend/src/routes/assignments.routes.ts
@@ -1,8 +1,9 @@
 import express from 'express'
-import { getAssignmentsForVehicle } from '../controllers/assignments.controller'
+import { getAssignmentsForVehicle, endAssignment } from '../controllers/assignments.controller'
 
 const router = express.Router()
 
 router.get('/assignments-vehicle/:id', getAssignmentsForVehicle)
+router.patch('/assignments-end/:id', endAssignment)
 
 export default router
