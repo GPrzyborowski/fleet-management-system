f9d9e68b4fc17a68a9d354b35f2a15f5c3e69062
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-05-31 18:03:20 +0200

    feat: add return and withdraw routes

--- backend/src/routes/vehicles.routes.ts
+++ backend/src/routes/vehicles.routes.ts
@@ -1,11 +1,20 @@
 import express from 'express'
-import { getVehicles, addVehicle, updateVehicle, deleteVehicle } from '../controllers/vehicles.controller'
+import {
+	getVehicles,
+	addVehicle,
+	updateVehicle,
+	returnToFleet,
+    withdrawFromFleet,
+	deleteVehicle,
+} from '../controllers/vehicles.controller'
 
 const router = express.Router()
 
 router.get('/vehicles', getVehicles)
 router.post('/vehicles', addVehicle)
-router.delete('/vehicles/:id', deleteVehicle)
 router.patch('/vehicles/:id', updateVehicle)
+router.patch('/vehicles/:id/return', returnToFleet)
+router.patch('/vehicles/:id/withdraw', withdrawFromFleet)
+router.delete('/vehicles/:id', deleteVehicle)
 
 export default router
