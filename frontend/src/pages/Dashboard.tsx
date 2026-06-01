dcf705f1892cb802068f312ec533f8d82715137c
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-06-02 00:53:54 +0200

    feat: fetch active assignments from database and display as a table

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,8 +1,10 @@
-import { useMemo } from 'react'
+import { useState, useEffect, useMemo, useCallback } from 'react'
 import { jwtDecode } from 'jwt-decode'
 import PageTransition from '../components/PageTransition'
 import Header from '../components/Header'
 import ActionCard from '../components/ActionCard'
+import ActiveAssignmentsTable from '../components/ActiveAssignmentsTable'
+import { API_URL } from '../config/api'
 
 interface TokenPayload {
 	id: number
@@ -11,7 +13,29 @@ interface TokenPayload {
 	exp: number
 }
 
+interface Vehicle {
+	brand: string
+	license_plate: string
+	model: string
+}
+
+interface Assignment {
+	dashboard_image_url: string
+	driver_id: number
+	end_fuel_level: number | null
+	end_mileage: number | null
+	end_time: string | null
+	id: number
+	start_fuel_level: number
+	start_mileage: number
+	start_time: string
+	status: string
+	vehicle_id: number
+	vehicles: Vehicle
+}
+
 export default function Dashboard() {
+	const [activeAssignments, setActiveAssignments] = useState<Assignment[]>([])
 	const token = localStorage.getItem('token')
 	const decoded = useMemo(() => {
 		if (!token) {
@@ -27,6 +51,26 @@ export default function Dashboard() {
 
 	const login = decoded?.login ?? ''
 	const role = decoded?.role ?? ''
+
+	const getActiveAssignments = useCallback(async () => {
+		try {
+			const res = await fetch(`${API_URL}/assignments`, {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			})
+			const data = await res.json()
+			setActiveAssignments(res.ok ? data : [])
+		} catch (err) {
+			console.error(err)
+		}
+	}, [token])
+
+	useEffect(() => {
+		if (role == 'driver') {
+			getActiveAssignments()
+		}
+	}, [role, getActiveAssignments])
 
 	return (
 		<PageTransition>
@@ -46,6 +90,11 @@ export default function Dashboard() {
 						links="vehicles"
 					/>
 				</div>
+			)}
+			{role == 'driver' && (
+				<>
+					<ActiveAssignmentsTable activeAssignments={activeAssignments} />
+				</>
 			)}
 		</PageTransition>
 	)
